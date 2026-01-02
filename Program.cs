e3e7d8680ce0bee6ff1094a3927bfca680f0e27a
zenin5479 <zenin5479@gmail.com>
2026-01-02 23:41:35 +0300

    Update 7.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -27,18 +27,17 @@ namespace SyncAppServer
                {
                   ConsoleKey key = Console.ReadKey(true).Key;
                   Console.WriteLine("Нажата клавиша: {0} Продолжить? (Y/N)", key);
-                  if (Console.ReadKey(true).Key != ConsoleKey.N)
-                  {
-                     // Ожидаем входящий запрос (блокирующий вызов)
-                     HttpListenerContext context = listener.GetContext();
-                     ProcessRequest(context);
-                  }
-                  else
+                  if (Console.ReadKey(true).Key == ConsoleKey.N)
                   {
                      exitLoop = true;
                      Console.WriteLine("Цикл прерван");
                   }
                }
+
+               // Ожидаем входящий запрос (блокирующий вызов)
+               HttpListenerContext context = listener.GetContext();
+               ProcessRequest(context);
+
             }
             catch (Exception ex)
             {
