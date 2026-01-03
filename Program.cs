ca85d9961e3ea2c57e009e24823b95e823b28735
zenin5479 <zenin5479@gmail.com>
2026-01-03 23:42:58 +0300

    Update 8.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace SyncAppServer
       {
          HttpListener listener = new HttpListener();
          // Указываем префиксы для прослушивания
-         string Prefixes = "http://127.0.0.1:8080/";
-         listener.Prefixes.Add("http://127.0.0.1:8080/");
+         string prefixes = "http://127.0.0.1:8080/";
+         listener.Prefixes.Add(prefixes);
          //listener.Prefixes.Add("http://127.0.0.1:8888/connection/");
          listener.Start();
          Console.WriteLine("Сервер запущен на http://127.0.0.1:8080/");
