34381bef51f91df3b0c627d9f10a0faf58a65630
zenin5479 <zenin5479@gmail.com>
2026-01-04 11:47:59 +0300

    Update 9.9 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -19,10 +19,9 @@ namespace SyncAppServer
          Console.WriteLine("Сервер запущен на {0}", prefixes);
          // Синхронная обработка запросов в цикле
 
-
          // Вариант 1 остановка цикла при нажатии клавиши "С"
-         Console.WriteLine("Цикл активен. Нажмите клавишу С для остановки");
-         bool exitLoop = false;
+         //Console.WriteLine("Цикл активен. Нажмите клавишу С для остановки");
+         //bool exitLoop = false;
          //while (!exitLoop)
          //{
          //   try
