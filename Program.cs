02d6bc396ed339790d813c9a9815ffc55c8954b1
zenin5479 <zenin5479@gmail.com>
2026-01-07 11:47:39 +0300

    Update 15.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -43,7 +43,6 @@ namespace SyncAppServer
             }
          }
 
-         //Console.ReadKey();
          // Считываем клавишу без вывода
          Console.ReadKey(intercept: true);
          // Очищаем буфер клавиши
